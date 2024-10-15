8a880549d94aa2612ae4f1d0c86eb487767b9404
izzat <izzatfadzlon@gmail.com>
2024-10-15 23:13:23 +0800

    successful render yt video

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback } from "react";
+import { View, Alert } from "react-native";
+import YoutubePlayer from "react-native-youtube-iframe";
 
 export default function App() {
+  const [playing, setPlaying] = useState(false);
+
+  const onStateChange = useCallback((state) => {
+    if (state === "ended") {
+      setPlaying(false);
+      Alert.alert("Video has finished playing!");
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <YoutubePlayer
+        height={300}
+        width={400}
+        play={playing}
+        videoId={"K4ZSmMHOH6o"}
+        onChangeState={onStateChange}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
